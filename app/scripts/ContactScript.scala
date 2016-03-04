c2ad09256ae72e1afdec5b7e59ccece71ea13961
Yang Bo <atryyang@thoughtworks.com>
2016-03-04 16:01:42 +0800

    Use Each to create scripts

--- app/scripts/ContactScript.scala
+++ app/scripts/ContactScript.scala
@@ -3,38 +3,32 @@ package scripts
 import commands.ContactCommand._
 
 import scalaz.std.list._
-import scalaz.syntax.traverse._
-import scalaz.syntax.monad._
+
+import com.thoughtworks.each.Monadic._
 
 object ContactScript {
 
-  def contactsScript: Script[xml.Elem] = {
-    toScript(GetEmailList).flatMap { emailList =>
-      emailList.traverseM[Script, xml.Elem] { email =>
-        if (email.matches("""[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}""")) {
-          toScript(GetContactNameByEmail(email)).map { name =>
-            List(<tr>
+  def contactsScript: Script[xml.Elem] = monadic[Script] {
+    val emailList = toScript(GetEmailList).each
+    <html>
+      <body>
+        <table>{
+          (for {
+            email <- emailList.monadicLoop // Converts emailList to a MonadicLoop
+            if email.matches("""[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}""")
+          } yield {
+            <tr>
               <td>
-                {name}
+                { toScript(GetContactNameByEmail(email)).each}
               </td>
               <td>
                 {email}
               </td>
-            </tr>)
-          }
-        } else {
-          (Nil: List[xml.Elem]).point[Script]
-        }
-      }.map { trs =>
-        <html>
-          <body>
-            <table>
-              {trs}
-            </table>
-          </body>
-        </html>
-      }
-    }
+            </tr>
+          }).underlying // Converts the MonadicLoop returned from for/yield comprehension to a List
+        }</table>
+      </body>
+    </html>
   }
 
 }
